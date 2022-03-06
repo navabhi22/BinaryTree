4d5b9ddaf2321bbd336dfde9a7ad2fcdfd203018
navabhi22 <navabhi22@gmail.com>
2022-03-07 02:47:29 +0530

    Abhinav Added BinaryTree.cs

---
+++ BinaryTree.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class BinaryTree 
+    {
+        public Node Root { get; set; }
+        public bool Add(int value)
+        {
+            Node before = null, after = this.Root;
+            while(after != null)
+            {
+                before = after;
+                if (value<after.Data)
+                {
+                    after = after.LeftNode;
+                }
+                else if (value>after.Data)
+                {
+                    after = after.RightNode;
+                }
+                return false;
+            }
+            Node newNode = new Node();
+            newNode.Data = value;
+
+            if (this.Root == null)
+            {
+                this.Root = newNode;
+            }
+            else
+            {
+                if (value < before.Data)
+                    before.LeftNode = newNode;
+                else
+                {
+                    before.RightNode = newNode; 
+                }
+            }
+            return true;
+        }
+        public void TravarsePreorder(Node Parent)
+        {
+            if (Parent != null)
+            {
+                Console.WriteLine(Parent.Data + " ");
+                TravarsePreorder(Parent.LeftNode);
+                TravarsePreorder(Parent.RightNode);
+            }
+        }
+    }
+}
