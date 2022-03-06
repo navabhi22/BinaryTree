58f4f7d6d7158179d7f84bcaba4d4422995e5484
navabhi22 <navabhi22@gmail.com>
2022-03-07 02:46:17 +0530

    Abhinav Added Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,13 @@ namespace BinarySearchTree
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to BinarySearchTree");
+            BinaryTree binaryTree = new BinaryTree();
+            binaryTree.Add(56);
+            binaryTree.Add(30);
+            binaryTree.Add(70);
+
+            binaryTree.TravarsePreorder(binaryTree.Root);
+
         }
     }
 }
